6624dcb608bdc72675acd64427377e6af0dc2103
imir-b <vladimir.bleskine@gmail.com>
2026-04-23 15:49:39 +0200

    feat(data): Add argument validation and refactor data initialization
    - Implement `ft_check_data` to validate all simulation parameters.
    - Create `ft_init_sems` to encapsulate semaphore initialization logic.
    - Refactor `ft_init_data` to use helper functions for clarity and error handling.
    - Initialize semaphore pointers to SEM_FAILED before use for robust cleanup.

--- philo_bonus/src/data.c
+++ philo_bonus/src/data.c
@@ -6,7 +6,7 @@
 /*   By: vlad <vlad@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 23:13:57 by vlad              #+#    #+#             */
-/*   Updated: 2026/04/23 15:24:22 by vlad             ###   ########.fr       */
+/*   Updated: 2026/04/23 15:49:16 by vlad             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,32 @@ void	*ft_free_data(t_data_bonus *data)
 	return (NULL);
 }
 
+int	ft_check_data(t_data_bonus *data)
+{
+	if (data->n_philosophers <= 0 || data->n_philosophers > MAX_PHILOSOPHERS
+		|| data->time_to_die <= 0 || data->time_to_eat <= 0
+		|| data->time_to_sleep <= 0)
+		return (1);
+	return (0);
+}
+
+int	ft_init_sems(t_data_bonus *data)
+{
+	sem_unlink("/philo_forks");
+	data->forks = sem_open("/philo_forks", O_CREAT, 0644, data->n_philosophers);
+	if (data->forks == SEM_FAILED)
+		return (1);
+	sem_unlink("/philo_print");
+	data->print_sem = sem_open("/philo_print", O_CREAT, 0644, 1);
+	if (data->print_sem == SEM_FAILED)
+		return (1);
+	sem_unlink("/philo_waiter");
+	data->waiter = sem_open("/philo_waiter", O_CREAT, 0644, 1);
+	if (data->waiter == SEM_FAILED)
+		return (1);
+	return (0);
+}
+
 t_data_bonus	*ft_init_data(int ac, char **av)
 {
 	t_data_bonus	*data;
@@ -34,6 +60,9 @@ t_data_bonus	*ft_init_data(int ac, char **av)
 	data = malloc(sizeof(t_data_bonus));
 	if (data == NULL)
 		return (NULL);
+	data->forks = SEM_FAILED;
+	data->print_sem = SEM_FAILED;
+	data->waiter = SEM_FAILED;
 	data->n_philosophers = ft_atoi(av[1]);
 	data->time_to_die = ft_atoi(av[2]);
 	data->time_to_eat = ft_atoi(av[3]);
@@ -42,18 +71,10 @@ t_data_bonus	*ft_init_data(int ac, char **av)
 		data->n_meals = ft_atoi(av[5]);
 	else
 		data->n_meals = -1;
+	if (ft_check_data(data))
+		return (free(data), NULL);
 	data->start_time = ft_get_timestamp();
-	sem_unlink("/philo_forks");
-	data->forks = sem_open("/philo_forks", O_CREAT, 0644, data->n_philosophers);
-	if (data->forks == SEM_FAILED)
-		return (free (data), NULL);
-	sem_unlink("/philo_print");
-	data->print_sem = sem_open("/philo_print", O_CREAT, 0644, 1);
-	if (data->print_sem == SEM_FAILED)
-		return (ft_free_data(data));
-	sem_unlink("/philo_waiter");
-	data->waiter = sem_open("/philo_waiter", O_CREAT, 0644, 1);
-	if (data->waiter == SEM_FAILED)
+	if (ft_init_sems(data))
 		return (ft_free_data(data));
 	return (data);
 }
